5de4250b97ece4c3139e1eb58e0640978db60baa
antsanch <antsanchez@gmail.com>
2014-04-05 10:56:31 -0600

    Eliminado el residuo de la configuracion a la carpeta SYM

--- config/ProjectConfiguration.class.php
+++ config/ProjectConfiguration.class.php
@@ -2,7 +2,6 @@
 require_once dirname(__FILE__).'/../lib/vendor/lessphp/lessc.inc.php';
 
 require_once dirname(__FILE__).'/../lib/vendor/symfony/lib/autoload/sfCoreAutoload.class.php';
-//require_once dirname(__FILE__).'/../SYM/symfony/lib/autoload/sfCoreAutoload.class.php';
 sfCoreAutoload::register();
 
 class ProjectConfiguration extends sfProjectConfiguration
