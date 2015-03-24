ac29e1a579fc16076e6f647b60d000b5b5031b6f
ramomar <omaarg@gmail.com>
2015-03-24 15:13:56 -0600

    Cambios esteticos en la página para reprogramar una cirugía

--- apps/frontend/modules/agenda/templates/reprogramarSuccess.php
+++ apps/frontend/modules/agenda/templates/reprogramarSuccess.php
@@ -1,3 +1,7 @@
-<h3>Reprogramar Cirugia</h3>
+<?php slot('titulo') ?>
+  <title>Reprogramar cirugía | SIGA-Qx </title>
+<?php end_slot() ?>
 
+<h3 class="page-title">Reprogramar cirguía</h3>
+<?php include_partial('qbreadcrumb', array('locacion' => 'Reprogramar')) ?>
 <?php include_partial('programarForm', array('form' => $form))?>
