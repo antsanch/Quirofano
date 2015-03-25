4b621f2af25851b554d45afecf39f839dfa1e4e6
ramomar <omaarg@gmail.com>
2015-03-24 18:02:36 -0600

    Agrega tooltips a la form para programar cirugía

--- apps/frontend/modules/agenda/templates/_programarForm.php
+++ apps/frontend/modules/agenda/templates/_programarForm.php
@@ -288,3 +288,9 @@
     </div>
   </div>
 </div>
+
+<script type="text/javascript">
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+</script>
--- lib/helper/programarCirugiaFormHelper.php
+++ lib/helper/programarCirugiaFormHelper.php
@@ -14,13 +14,13 @@
 /*
 * Con esta funcion renderizamos un icono en el lado derecho del input
 * cuando se pasa el puntero por encima del icono una tooltip mostrara 
-* el error
+* el erroi
 * Los parametros que requiere es el objeto $form['campo']
 */
 function renderErrorIcon($campo) {
   $html = "";
   if ($campo->hasError()) {
-    $html = "<i class='fa fa-warning tooltips' data-placement='auto' data-original-title='{$campo->getError()}'></i>";
+    $html = "<i class='fa fa-warning' data-toggle='tooltip' data-placement='auto left' data-container='form' data-title='{$campo->getError()}'></i>";
   }
   echo $html;
 }
