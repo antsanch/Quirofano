8147ec53dde535f33759370114768fe26f9635b0
ramomar <omaarg@gmail.com>
2015-04-28 14:17:15 -0500

    Modifica pestaña cirguías diferidas

--- apps/frontend/modules/agenda/templates/diferidasSuccess.php
+++ apps/frontend/modules/agenda/templates/diferidasSuccess.php
@@ -1,57 +1,67 @@
 <?php use_stylesheet('global/styleAgenda.css') ?>
 <?php use_helper('agenda') ?>
 
+<?php slot('titulo') ?>
+  <title>Agenda de <?php echo $Quirofano['Nombre'] ?> | SIGA-HU </title>
+<?php end_slot() ?>
+
 <h3>Agenda de procedimientos en <?php echo $Quirofano['Nombre'] ?></h3>
 <?php include_partial('qbreadcrumb', array('locacion' => 'Cirugías diferidas')) ?>
 <?php include_partial('menuShow', array('Cirugias' => $Cirugias, 'Quirofano' => $Quirofano, "date" => $date)) ?>
 
-<div id="camasPanel">
-</div>
-
-<table id="agenda" border="0" width="100%" cellspacing="0">
 <!--Script para mostrar alertas-->
 <script type="text/javascript">
 function saludo() {alert('Programación Exitosa')}
 function verificar() {alert('Verificar la hora')}
 </script>
-<!--Script para mostrar alertas-->
 
 <!--Mostrar alertas-->
 <?php if ($sf_user->hasFlash('notice')): ?>
 <?php if ($sf_user->getFlash('notice') == 'Verificar la hora' ):?>
 
-<script type="text/javascript">
-function start() {verificar()}
-window.onload = start;
-</script>
+	<script type="text/javascript">
+	function start() {verificar()}
+	window.onload = start;
+	</script>
 
 <?php else: ?>
    <!-- <div class="flash_notice"><?php echo $sf_user->getFlash('notice') ?></div>-->
 
-<script type="text/javascript">
-function start() {saludo()}
+	<script type="text/javascript">
+	function start() {saludo()}
 
-window.onload = start;
-</script>
+	window.onload = start;
+	</script>
 
 <?php endif; ?>
 <?php endif; ?>
 
 <!-- Mostrar alertas-->
 <?php $title = null ?>
-<?php foreach($Cirugias as $c): ?>
 
-<?php if ($c->getCancelada() != 1): ?>
-<?php if ($c->getStatus() != $title): ?>
-<?php echo print_head($c) ?>
-  <td colspan="11"><h3 style="padding-top: 11px;"><?php echo $c->getVerboseStatus() ?></h3></td>
-  <?php $title = $c->getStatus() ?>
-<?php endif; ?>
-<?php include_partial('agendaQuirofano', array('cirugia' => $c, 'slug' => $Quirofano['Slug'])) ?>
-<?php endif; ?>
-<?php endforeach; ?>
+<div class="table-responsive">
+	<table id="agenda" class="table table-hover table-striped">
+    	<tbody>
+		<?php foreach($Cirugias as $c): ?>
+			<?php if ($c->getCancelada() != 1): ?>
+			<?php if ($c->getStatus() != $title): ?>
+			<?php echo print_head($c->getVerboseStatus()) ?>
+		  	<?php $title = $c->getStatus() ?>
+			<?php endif; ?>
+		<?php echo renderProgramada($c); ?>
+		<?php endif; ?>
+		<?php endforeach; ?>
+		</tbody>
+	</table>
+</div>
 
-
-<!-- agregado -->
-
-</table>
+<script type="text/javascript">
+$(function () {
+  $('[data-toggle="popover"]').popover({
+    trigger: 'hover',
+    html: true, 
+    delay: { "show": 0, "hide": 1 },
+    placement: 'auto top'
+  });
+});
+</script>
